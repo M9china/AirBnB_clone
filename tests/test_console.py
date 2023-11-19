54c30d816c24114aa18fb6c1b5a50a7f17b59a01
M9china <nqobilencaza2@gmail.com>
2023-11-19 10:55:46 +0200

    refactor:optimized code

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,162 +1,126 @@
+import sys
 import unittest
 from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
+from models import storage
+from models.basemodel import BaseModel
 
 
-class TestHBNBCommand(unittest.TestCase):
-    """
-    Test cases for the HBNBCommand class methods.
-    """
+class TestConsole(unittest.TestCase):
 
     def setUp(self):
-        """
-        Set up a new HBNBCommand instance for each test method.
-        """
-        self.cmd = HBNBCommand()
+        self.stdout_patch = patch('sys.stdout', new_callable=StringIO)
+        self.mock_stdout = self.stdout_patch.start()
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_create(self, mock_stdout):
-        """
-        Test for 'do_create' method with valid class input.
-        Expected output: ID of the created instance
-        """
-        with patch('models.engine.file_storage.new') as mock_storage_new:
-            self.cmd.onecmd("create BaseModel")
-            mock_storage_new.assert_called_once()
-            self.assertTrue(len(mock_stdout.getvalue().strip()) > 0)
+    def tearDown(self):
+        self.stdout_patch.stop()
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_show(self, mock_stdout):
-        """
-        Test for 'do_show' method with a valid class and ID input.
-        Expected output: String representation of the instance.
-        """
-        with patch('models.engine.file_storage.all') as mock_storage_all:
-            mock_storage_all.return_value = {"BaseModel.123": "instance"}
-            self.cmd.onecmd("show BaseModel 123")
-            self.assertEqual(mock_stdout.getvalue().strip(), "instance")
+    def test_create_command(self):
+        with patch('builtins.input',
+                   return_value="create BaseModel") as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("create BaseModel")
+            self.assertIn("{}".format(
+                storage.new.call_args[0][0].id),
+                self.mock_stdout.getvalue().strip())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_destroy(self, mock_stdout):
-        """
-        Test for 'do_destroy' method with a valid class and ID input.
-        Expected output: No output (should print nothing).
-        """
-        with patch('models.engine.file_storage.all') as mock_storage_all:
-            mock_storage_all.return_value = {"BaseModel.123": "instance"}
-            with patch('models.storage.Storage.save') as mock_storage_save:
-                self.cmd.onecmd("destroy BaseModel 123")
-                mock_storage_save.assert_called_once()
-                self.assertEqual(mock_stdout.getvalue().strip(), "")
+    def test_show_command(self):
+        with patch('builtins.input',
+                   return_value="show BaseModel") as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("show BaseModel")
+            self.assertIn("** instance id missing **",
+                          self.mock_stdout.getvalue().strip())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_all(self, mock_stdout):
-        """
-        Test for 'do_all' method with a valid class input.
-        Expected output: String representation of all instances of the class.
-        """
-        with patch('models.engine.file_storage.all') as mock_storage_all:
-            mock_storage_all.return_value = {"BaseModel.123": "instance"}
-            self.cmd.onecmd("all BaseModel")
-            self.assertEqual(mock_stdout.getvalue().strip(), "instance")
+        with patch('builtins.input', return_value="show BaseModel {}".format(
+                storage.new(BaseModel()).id)) as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("show BaseModel {}".format(
+                storage.new.call_args[0][0].id))
+            self.assertIn(str(
+                storage.new.call_args[0][0]),
+                self.mock_stdout.getvalue().strip())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_update(self, mock_stdout):
-        """
-        Test for 'do_update' method with a valid class,
-        ID, attribute, and value input.
-        Expected output: No output (should print nothing).
-        """
-        with patch('models.engine.file_storage.all') as mock_storage_all:
-            mock_storage_all.return_value = {"BaseModel.123": "instance"}
-            with patch('models.storage.Storage.new') as mock_storage_new:
-                with patch('models.storage.Storage.save') as mock_storage_save:
-                    self.cmd.onecmd("update BaseModel 123 name 'NewName'")
-                    mock_storage_new.assert_called_once()
-                    mock_storage_save.assert_called_once()
-                    self.assertEqual(mock_stdout.getvalue().strip(), "")
+    def test_destroy_command(self):
+        with patch('builtins.input',
+                   return_value="destroy BaseModel") as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("destroy BaseModel")
+            self.assertIn("** instance id missing **",
+                          self.mock_stdout.getvalue().strip())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_default_command(self, mock_stdout):
-        """
-        Test for 'default' method with a valid command.
-        Expected output: Custom output based on the command.
-        """
-        with patch('models.engine.file_storage.all') as mock_storage_all:
-            mock_storage_all.return_value = {"BaseModel.123": "instance"}
-            self.cmd.onecmd("show BaseModel 123")
-            self.assertEqual(mock_stdout.getvalue().strip(), "instance")
+        instance_id = storage.new(BaseModel()).id
+        with patch('builtins.input',
+                   return_value="destroy BaseModel {}".format(
+                instance_id)) as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("destroy BaseModel {}".format(
+                instance_id))
+            self.assertNotIn(instance_id, storage.all())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_default_command_invalid(self, mock_stdout):
-        """
-        Test for 'default' method with an invalid command.
-        Expected output: Error message.
-        """
-        self.cmd.onecmd("invalid_command")
-        expected_output = "** NO COMMAND FOR invalid_command**"
-        self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
+    def test_all_command(self):
+        with patch('builtins.input', return_value="all") as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("all")
+            self.assertIn("BaseModel",
+                          self.mock_stdout.getvalue().strip())
 
-    def test_do_count(self):
-        """
-        Test for 'do_count' method with a valid class input.
-        Expected output: Number of instances of the class.
-        """
-        with patch('models.engine.file_storage.all') as mock_storage_all:
-            mock_storage_all.return_value = {"BaseModel.123": "instance"}
-            with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                self.cmd.onecmd("count BaseModel")
-                self.assertEqual(mock_stdout.getvalue().strip(), "1")
+        with patch('builtins.input',
+                   return_value="all BaseModel") as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("all BaseModel")
+            self.assertIn("BaseModel",
+                          self.mock_stdout.getvalue().strip())
 
-    def test_do_count_nonexistent_class(self):
-        """
-        Test for 'do_count' method with a non-existent class input.
-        Expected output: Error message.
-        """
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.cmd.onecmd("count NonExistentClass")
-            expected_output = "** class doesn't exist **"
-            self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
+    def test_update_command(self):
+        with patch('builtins.input',
+                   return_value="update BaseModel") as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("update BaseModel")
+            self.assertIn("** instance id missing **",
+                          self.mock_stdout.getvalue().strip())
 
-    def test_do_count_missing_class(self):
-        """
-        Test for 'do_count' method with a missing class input.
-        Expected output: Error message.
-        """
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.cmd.onecmd("count")
-            expected_output = "** Class name missing **"
-            self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
+        instance_id = storage.new(BaseModel()).id
+        with patch('builtins.input',
+                   return_value="update BaseModel {}".format(instance_id)) as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with(
+                "update BaseModel {}".format(instance_id))
+            self.assertIn("** attribute name missing **",
+                          self.mock_stdout.getvalue().strip())
 
-    def test_do_quit(self):
-        """
-        Test for 'do_quit' method.
-        Expected output: 'Left the HBNBCommand Shell'.
-        """
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.assertTrue(self.cmd.do_quit(''))
-            self.assertEqual(mock_stdout.getvalue(
-            ).strip(), "Left the HBNBCommand Shell")
+        with patch('builtins.input',
+                   return_value="update BaseModel {} name".format(instance_id)) as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with(
+                "update BaseModel {} name".format(instance_id))
+            self.assertIn("** value missing **",
+                          self.mock_stdout.getvalue().strip())
 
-    def test_do_EOF(self):
-        """
-        Test for 'do_EOF' method.
-        Expected output: 'Left the HBNBCommand Shell'.
-        """
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.assertTrue(self.cmd.do_EOF(''))
-            self.assertEqual(mock_stdout.getvalue().strip(),
-                             "Left the HBNBCommand Shell")
+        with patch('builtins.input',
+                   return_value="update BaseModel {} name 'NewName'".format(instance_id)) as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with(
+                "update BaseModel {} name 'NewName'".format(instance_id))
+            self.assertEqual(storage.new.call_args[0][0].name, "NewName")
 
-    def test_emptyline(self):
-        """
-        Test for 'emptyline' method.
-        Expected output: False.
-        """
-        self.assertFalse(self.cmd.emptyline())
+    def test_count_command(self):
+        with patch('builtins.input', return_value="count BaseModel") as mock_input:
+            HBNBCommand().cmdloop()
+            mock_input.assert_called_with("count BaseModel")
+            self.assertIn("1", self.mock_stdout.getvalue().strip())
+
+    def test_quit_command(self):
+        with patch('builtins.input', return_value="quit") as mock_input:
+            with self.assertRaises(SystemExit):
+                HBNBCommand().cmdloop()
+
+    def test_EOF_command(self):
+        with patch('builtins.input', return_value="EOF") as mock_input:
+            with self.assertRaises(SystemExit):
+                HBNBCommand().cmdloop()
 
 
 if __name__ == '__main__':
-
     unittest.main()
